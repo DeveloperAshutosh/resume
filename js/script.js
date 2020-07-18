ea95c8b8fa5ffd5e7f0960af13990c23f6de058e
dimple sharma <dimplessharma@hotmail.com>
2020-07-18 14:36:38 -0600

    swear word blocker method added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,47 @@
 
-
-
-
 document.querySelector("#submit").addEventListener("click",(e)=>{
-    let sWord = ["feldercarb", "frack", "skinjob", "vulgacarb"];
-    const message  = document.querySelector("#message");
-    const label  = document.querySelector("#messageaWarning");
-        let i;
-    for(  i = 0  ; i <= sWord.length; i++ ){
-       let errors = false;
-        
-        if( sWord.includes(message)){
-            label.innerHTML = "Please be professional";
-            
-           
-           errors = true;
-            
-        }
-        
-        else{
-            mailTo();
-            
-        }
-        
-
-    }
+    e.preventDefault();
+    swordblocker();
+    
+    
     
 });
-function mailTo(){
+function mailTo(e){
     const subject = document.querySelector("#subject").value.trim();
     const message = document.querySelector("#message").value.trim();
     window.location.href = "mailto:akausha1@ualberta.ca?subject=" + subject + "&body=" + message ;
+   console.log( "mailTo running..." );
+}
+function swordblocker(){
+    let sWord = ["feldercarb", "frack", "skinjob", "vulgacarb"];
+    console.log( "swordblocker running..." );
+   
+    const message  = document.querySelector("#message");
+    const label  = document.querySelector(".messageWarning");
+        let i;
+        let errors = false;
+    
+    for(  i = 0  ; i < sWord.length; i++ ){
+        
+        
+        if( message.value.includes(sWord[i])){
+           
+            console.log("message is" + message.value);
+            
+            errors = true ;
+        } 
+        
+    }
+    if(errors === true){
+        label.innerHTML = "Please be professional";
+        message.value = "";
+        message.focus();
+    }
+    else{
+        label.innerHTML="";
+        mailTo();
+    }
+       
 }
 
 
