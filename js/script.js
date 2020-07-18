ee3bd8f5a7727914b336b0e5cc37bfd3cc4c0599
dimple sharma <dimplessharma@hotmail.com>
2020-07-17 21:15:16 -0600

    added mailto using js

--- js/script.js
+++ js/script.js
@@ -8,8 +8,18 @@ const nameWarning = document.querySelector(".firstNameWarning");
 const emailWarning = document.querySelector(".emailWarning");
 const subjectWarning = document.querySelector(".subjectWarning");
 const messageWarning = document.querySelector(".messageWarning");
+let sWords = [];
+
+submit.addEventListener("click",()=>{
 
 
+
+mailTo();
+
+});
+function mailTo(){
+    window.location.href = "mailto:akausha1@ualberta.ca   "  + "?subject=" + subject.value + "&body=" +message.innerHTML;
+}
 
 
 
