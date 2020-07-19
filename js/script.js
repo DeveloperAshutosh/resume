dcff70de43ee508cb95ba57a6a7541f7ff77c362
dimple sharma <dimplessharma@hotmail.com>
2020-07-18 18:23:42 -0600

    condition added to check if all the inputs are empty

--- js/script.js
+++ js/script.js
@@ -1,47 +1,75 @@
-
+//diricting to mail after clicking
 document.querySelector("#submit").addEventListener("click",(e)=>{
     e.preventDefault();
-    swordblocker();
     
+    SwearWordBlocker();//calling the swear word blocker method
     
-    
+ 
 });
-function mailTo(e){
-    const subject = document.querySelector("#subject").value.trim();
-    const message = document.querySelector("#message").value.trim();
+//creating the function to direct it to the default email
+function MailTo(){
+    const subject = document.querySelector("#subject").value.trim(); //assigning the subject value of the subject input
+    const message = document.querySelector("#message").value.trim(); //assigning the message the input value of message
     window.location.href = "mailto:akausha1@ualberta.ca?subject=" + subject + "&body=" + message ;
-   console.log( "mailTo running..." );
-}
-function swordblocker(){
-    let sWord = ["feldercarb", "frack", "skinjob", "vulgacarb"];
-    console.log( "swordblocker running..." );
    
+}
+//creating the function to filter the input from the user
+function SwearWordBlocker(){
+    let swearWord = ["feldercarb", "frack", "skinjob", "vulgacarb"];//setting the bad words
+    
+    const name = document.querySelector("#name");
+    const email = document.querySelector("#email");
+    const nameWarning = document.querySelector(".nameWarning");
+    const emailWarning = document.querySelector(".emailWarning");
+    const messageWarning  = document.querySelector(".messageWarning");
     const message  = document.querySelector("#message");
-    const label  = document.querySelector(".messageWarning");
+    const subjectWarning  = document.querySelector(".subjectWarning");
+    
         let i;
         let errors = false;
-    
-    for(  i = 0  ; i < sWord.length; i++ ){
+    //looping through the message to check if there is any bad word
+    for(  i = 0  ; i < swearWord.length; i++ ){
         
         
-        if( message.value.includes(sWord[i])){
+        if( message.value.includes(swearWord[i])){
            
-            console.log("message is" + message.value);
+            
             
             errors = true ;
         } 
         
     }
+    
+    //If there is bad word then set the warning message
     if(errors === true){
-        label.innerHTML = "Please be professional";
-        message.value = "";
-        message.focus();
+        messageWarning.innerHTML = "*Please be professional this is not acceptable";
+        
     }
+    //If name is not entered then display warning
+    else if(name.value.trim() === "" || email.value.trim() === "" || subject.value.trim() ==="" ){
+        if(name.value.trim() === ""){
+            nameWarning.innerHTML = "*name is missing";
+
+        }
+         // If email is missing then display the warning
+    
+        if (email.value.trim() === ""){
+            emailWarning.innerHTML = "*email is missing";
+
+        }
+        if(subject.value.trim() === ""){
+            subjectWarning.innerHTML = "*subject is missing"
+        }
+
+
+        
+    }
+   
     else{
-        label.innerHTML="";
-        mailTo();
+        MailTo();
     }
-       
+
+      
 }
 
 
